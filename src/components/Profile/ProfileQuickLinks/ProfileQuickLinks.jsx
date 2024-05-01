4d163e61bf959b0e533fcc12819f26fa9ee3b921
Valentine Omonya <valomosh254@gmail.com>
2024-05-01 10:39:00 +0300

    Added Profile Quick Links

--- src/components/Profile/ProfileQuickLinks/ProfileQuickLinks.jsx
+++ src/components/Profile/ProfileQuickLinks/ProfileQuickLinks.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import "./ProfileQuickLinks.css"
+import WaletFundsLink from '../../Walet/WaletFundsLink/WaletFundsLink'
 const ProfileQuickLinks = () => {
   return (
-    <div>
-      
+    <div className='profile-quick-links-container'>
+        <div className='quick-links-center'></div>
+      <WaletFundsLink/>
+      <WaletFundsLink/>
+      <WaletFundsLink/>
+      <WaletFundsLink/>
+      <WaletFundsLink/>
+      <WaletFundsLink/>
     </div>
   )
 }
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import MainContainer from '../../components/MainContainer/MainContainer'
 import ProfileInfo from '../../components/Profile/ProfileInfo/ProfileInfo'
 import ProfileIncome from '../../components/Profile/ProfileIncome/ProfileIncome'
-
+import ProfileQuickLinks from '../../components/Profile/ProfileQuickLinks/ProfileQuickLinks'
+import "./Profile.css"
 const Profile = () => {
   return (
   <MainContainer>
+    <div className='profile-page-container'>
     <ProfileInfo/>
     <ProfileIncome/>
+    <ProfileQuickLinks/>
+    </div>
   </MainContainer>
   
   )

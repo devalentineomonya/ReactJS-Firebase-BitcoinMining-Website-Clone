2fdfea6e99e4b3d1f07e09dec613b185c3f74529
Valentine Omonya <valomosh254@gmail.com>
2024-05-01 10:45:20 +0300

    Added conditional redering to navbar

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -2,18 +2,25 @@ import React, { useRef } from "react";
 import "./MainContainer.css";
 import Navbar from "../Navbar/Navbar";
 
-const MainContainer = ({ children,pb="70px"}) => {
-  const ContentContainer= useRef();
-
+const MainContainer = ({ children, pb = "70px", showNav = true }) => {
+  const ContentContainer = useRef();
 
   return (
     <main className="main-container">
       <div className="main-content-container">
-        <div ref={ContentContainer} className="content-container" style={{paddingBottom:pb}}>{children}</div>
+        <div
+          ref={ContentContainer}
+          className="content-container"
+          style={{ paddingBottom: pb }}
+        >
+          {children}
+        </div>
       </div>
-      <div className="navbar-container">
-        <Navbar />
-      </div>
+      {showNav && (
+        <div className="navbar-container">
+          <Navbar />
+        </div>
+      )}
     </main>
   );
 };
--- src/components/SpinnerLoader/SpinnerLoader.jsx
+++ src/components/SpinnerLoader/SpinnerLoader.jsx
@@ -4,10 +4,7 @@ import loadingImage from "../../assets/Images/SiteImages/SiteIcons/loading.png"
 import MainContainer from '../MainContainer/MainContainer'
 const SpinnerLoader = () => {
   return (
-    <MainContainer pb="0px">
-
-
-
+    <MainContainer pb="0px" showNav={false}>
     <div className='loading-container'>
       <img src={loadingImage} alt="" />
     </div>
